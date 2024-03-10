b540656aa6a3792fb58178d4514314fdd66b8b1d
springhan <3379184879@qq.com>
2024-03-10 16:14:06 +0800

    Modified font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monego:pixelsize=40:antialias=true:autohint=true";
+static char *font = "Monego:pixelsize=37:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
